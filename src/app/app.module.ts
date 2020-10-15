f6b91cea3c2240fd1efd12ba725121bcced7c91a
luisg <luisgodoy0100@gmail.com>
2020-10-15 00:21:40 -0600

    crear pagina pagar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagarComponent } from './pagar/pagar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagarComponent
   ],
   imports: [
     BrowserModule,
