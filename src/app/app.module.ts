62afd3761049607062c5f1d6bf0f49f0465c05f1
luisg <luisgodoy0100@gmail.com>
2020-10-24 13:10:49 -0600

    comprar tarjeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,24 @@ import { AppComponent } from './app.component';
 import { PagarComponent } from './pagar/pagar.component';
 import { ListadoComponent } from './listado/listado.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
 
 
 import {MatInputModule} from '@angular/material/input';
 import { MatCardModule} from "@angular/material/Card";
+import {MatDatepickerModule} from '@angular/material/datepicker';
+/* import {MatNativeDateModule} from '@angular/material/nativeDateModule';
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+ */
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { MatListModule} from "@angular/material/List";
 import { MatButtonModule} from "@angular/material/Button";
+import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
+
+import { MatRippleModule} from '@angular/material/core';
 
 
 @NgModule({
@@ -30,7 +40,13 @@ import { MatButtonModule} from "@angular/material/Button";
     HttpClientModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDatepickerModule,
+   MatNativeDateModule ,
+   ReactiveFormsModule,
+   MatRippleModule,
+   FormsModule,
+   MatSelectModule
 
   ],
   providers: [],
--- src/app/pagar/pagar.component.ts
+++ src/app/pagar/pagar.component.ts
@@ -1,19 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder,  Validators } from '@angular/forms';
+ 
+angForm: FormGroup;
 @Component({
   selector: 'app-pagar',
   templateUrl: './pagar.component.html',
   styleUrls: ['./pagar.component.scss']
 })
 export class PagarComponent implements OnInit {
+  angForm: FormGroup;
 
-  constructor() { }
+  constructor( private route: ActivatedRoute,private fb:FormBuilder) { 
+
+
+  }
 
   ngOnInit(): void {
+    var id = this.route.snapshot.paramMap.get('id');
+  this. setdata()
   }
+
+  nombre:string=""
+  imagen:string="https://media.karousell.com/media/photos/products/2020/5/21/rm50_goggle_play_gift_card_mal_1590040469_c1100b5a_progressive.jpg"
+  status:string=""
+  id:string=""
+  notarjeta:string=""
+  charge:string=""
+  codigov:string=""
+  namcliente:string=""
+  total:string=""
+  startDate = new Date(1990, 0, 1);
+
+  cards: Cards[] = [
+    {value: '1', viewValue: 'Visa'},
+    {value: '2', viewValue: 'Master Card'},
+    {value: '3', viewValue: 'Dinner club'}
+  ];
+  setdata(){
+    this.nombre="regalo 1"
+
+    this.imagen="https://media.karousell.com/media/photos/products/2020/5/21/rm50_goggle_play_gift_card_mal_1590040469_c1100b5a_progressive.jpg"
+    this.status="Active"
+    this.id="1"
+    this.notarjeta="6-7"
+    this.charge="6-7"
+    this.codigov=""
+    this.namcliente=""
+    this.total="455";
+
+  }
+
+  Comprar() {
+   console.log("notarjeta es "+this.notarjeta);
+   console.log("codigov es "+this.codigov);
+   console.log("namcliente es "+this.namcliente);
+   console.log("total es "+this.total);
+   console.log("notarjeta es "+this.notarjeta);
+   console.log("startDate es "+this.startDate);
+/*    
+   console.log("total es "+this.total);
+   console.log("notarjeta es "+this.notarjeta); */
+  }
+
+
   pagar(notarjeta,Nombretarjeta,Fechaexpiración,Codigoverificador,Montoapagar,Moneda){
 
     throw new Error('fallo pago')
 
   }
+}
+interface Cards {
+  value: string;
+  viewValue: string;
 }
