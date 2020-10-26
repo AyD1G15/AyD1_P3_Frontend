edeb66e30336e57ba776f14c8351708be2e116d3
luisg <luisgodoy0100@gmail.com>
2020-10-25 20:44:20 -0600

    se hacen cambios en listado

--- src/app/listado/listado.component.ts
+++ src/app/listado/listado.component.ts
@@ -1,4 +1,5 @@
 
+
 import { Component, OnInit } from '@angular/core';
 // import { System } from 'system';
 import {HttpClient} from "@angular/common/http";
@@ -11,12 +12,8 @@ import {ServicioService} from '../servicio.service';
   styleUrls: ['./listado.component.scss']
 })
 export class ListadoComponent implements OnInit {
-  regalos:any;
-  mydata:Element ;
-  dataSource: any[] = [];
-  cantidad="1"
-  active1=""
-  card1=""
+  regalos;
+
   constructor(private http: HttpClient,private servicio:ServicioService) { }
 
   ngOnInit(): void {
@@ -27,37 +24,15 @@ export class ListadoComponent implements OnInit {
 
       var dat:any=data
       this.regalos=data;
-    this.show("JSON ES")
-    this.show(JSON.stringify(data))
-    this.show("ob.active1 es ")
+    
 
-      if(data!=null){
-
-         let ob:any=data[0];
-        this.show("ob.active es "+ob.active)
-        this.active1=ob.active
-       if(this.regalos[0].availability!=null){
-          this.show("avaliabily")
-
-          this.dataSource = [];
-
-          this.regalos[0].availability.forEach(element => {
-            this.show("id es "+element.id)
-
-            this.dataSource.push(this.createnewrow(element.id,element.total));
-           });
-         }else this.show("es nulo availabiliy")
-      }
 
 
       });
  
 
   }
-show(a){
-  console.log(a)
 
-}
   
   retorno_objecto(){
      return this.http.get('./../listado/archivo.json');
@@ -69,51 +44,16 @@ show(a){
     throw new Error('fallo seleccion de regalo')
 
   }
-  cupones(numeros){
 
-    if ( !isNaN(numeros) ) {
-      return "true"
-
-    }else return "false"
-
-  }
 
   Comprar(regaloid){
-   this.show("cantidad es "+ this.cantidad)
-   this.show("regaloid es "+ regaloid)
-   this.show("card1 es "+ this.card1)
+
    // this.servicio.message("el id es "+regaloid,"success")
 
-     this.servicio.navegar('pagar');
+    this.servicio.navegar('pagar');
 
   }
 
 
-  createnewrow(id,existencia){
 
-   
-    const dat:Element={
-
-      id,
-      existencia
-      
-     }
-
-     return dat
-     
-     
-   }
-
-
-
-
-}
-
-
-
-export interface Element {
-  id: string;
-  existencia: string;
-  
-  
 }
